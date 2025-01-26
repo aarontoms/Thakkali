1602a0de802f30c2a12462fb5fd265afb3bef8fe
aarontoms <aarontoms29@gmail.com>
2025-01-26 23:09:23 +0530

    login and signup

--- server/a.py
+++ server/a.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 import bcrypt, os
+from datetime import datetime
 from dotenv import load_dotenv
 
 
@@ -28,17 +29,44 @@ def handle_options(path=None):
     response.headers['Access-Control-Max-Age'] = '86400'
     return response
 
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    
-    
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    return 'Signup'
+    data = request.get_json()
+    username = data['username'].lower() 
+    email = data['email']
+    password = data['password']
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    user = db.Auth.find_one({"username": username})
+    if user:
+        return jsonify({"error": "Username already exists"}), 400
+    else:
+        user = {
+            "username": username,
+            "email": email,
+            "password": hashed,
+            "created_at":datetime.now()
+        }
+        db.Auth.insert_one(user)
+        user = db.Auth.find_one({"username": username})
+        return jsonify({"message": "Signup successful", "userid": str(user["_id"]), "username":user["username"]}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username').lower()
+    password = data.get('password')
+    user = db.Auth.find_one({"username": username})
+    if user:
+        if bcrypt.checkpw(password.encode('utf-8'), user['password']):
+            userid = str(db.Auth.find_one({"username": username})["_id"])
+            return jsonify({"message": "Login successful", "userid": userid, "username": username}), 200
+        else:
+            return jsonify({"error": "Invalid password"}), 401
+    else:
+        return jsonify({"error": "User not found"}), 404
+    
+
 
 
 
